b561468945e6e6d8845bd97ec241afd421b00842
AnandaCahyaRamadan <76020880+AnandaCahyaRamadan@users.noreply.github.com>
2026-01-20 16:05:47 +0700

    add docker

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -4,13 +4,23 @@ import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'localhost',
+    //   port: 5432,
+    //   username: 'postgres',
+    //   password: '',
+    //   database: 'todo_db',
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    // }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5416,
-      username: 'postgres',
-      password: '',
-      database: 'db_todo',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
     }),
--- be/src/main.ts
+++ be/src/main.ts
@@ -1,9 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Aktifkan validator
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  // Aktifkan CORS
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,POST,PATCH,DELETE',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
-  app.enableCors();
 }
+
 bootstrap();
